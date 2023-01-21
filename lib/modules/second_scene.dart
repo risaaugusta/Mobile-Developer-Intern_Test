5ea61cca7a9e9f92714bda7673160d9d4605bb61
risaaugusta <augustariris20@gmail.com>
2023-01-21 08:22:39 +0700

    fixing layout

--- lib/modules/second_scene.dart
+++ lib/modules/second_scene.dart
@@ -28,6 +28,7 @@ class _SecondSceneState extends State<SecondScene> {
         margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         height: MediaQuery.of(context).size.height,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Column(
@@ -39,14 +40,16 @@ class _SecondSceneState extends State<SecondScene> {
               ],
             ),
             Container(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 250),
-              child: Center(child: Text(Global.selectedName,textAlign: TextAlign.center,style: TextStyle(color: Colors.black, fontFamily:Fonts.BOLD,fontSize: 20))),
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              height: 500,
+              child: Center(child: Text(
+                  Global.selectedName == '' ? 'Selected User Name' : Global.selectedName,
+                  textAlign: TextAlign.center,style: TextStyle(color: Colors.black, fontFamily:Fonts.BOLD,fontSize: 20))),
             ),
             Container(
-              margin:  EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+              margin:  EdgeInsets.symmetric(horizontal: 20),
               height: 55,
-              child: Container(
-                  child:
+              child:
                   ElevatedButton(
                     style: ButtonStyle.raisedButtonStyle,
                     onPressed: () {
@@ -57,7 +60,6 @@ class _SecondSceneState extends State<SecondScene> {
                     },
                     child: Text('Choose a user',style: TextStyle(color: Colors.white, fontFamily:Fonts.MEDIUM,fontSize: 12)),
                   )
-              ),
             ),
           ],
         ),
